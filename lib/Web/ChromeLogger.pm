d9a65722c447b1f676e7c85324d37e02b4d97fa4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-03-27 20:45:52 +0900

    Initial import

---
+++ lib/Web/ChromeLogger.pm
@@ -0,0 +1,39 @@
+package Web::ChromeLogger;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Web::ChromeLogger - It's new $module
+
+=head1 SYNOPSIS
+
+    use Web::ChromeLogger;
+
+=head1 DESCRIPTION
+
+Web::ChromeLogger is ...
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom@gmail.comE<gt>
+
+=cut
+
